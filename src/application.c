6cbf356494ad9cbe164ced17187c2a925ee162de
Ducky <masterless123@gmail.com>
2021-11-16 11:34:41 -0500

    Use input to determine sleep time

--- src/application.c
+++ src/application.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define MAX_BUFFER_LENGTH 8
+#define CLEARSCREEN printf("\e[1;1H\e[2J")
+
+void start_timer (unsigned int seconds)
+{
+  fflush (stdout);
+  sleep (seconds);
+}
 
 int main (void)
 {
+  CLEARSCREEN;
+  
   char active_time[MAX_BUFFER_LENGTH];
-  char* break_time[MAX_BUFFER_LENGTH];
+  char break_time[MAX_BUFFER_LENGTH];
 
-  printf ("Enter minutes for active time: ");
+  printf ("> Enter minutes for active time:\t");
   fgets (active_time, MAX_BUFFER_LENGTH, stdin);
+  printf ("> Enter minutes for break time:\t\t");
+  fgets (break_time, MAX_BUFFER_LENGTH, stdin);
 
-  printf ("Enter minutes for break time: ");
-  fgets (active_time, MAX_BUFFER_LENGTH, stdin);
+  CLEARSCREEN;
+
+  while (1)
+    {
+      // Active timer
+      printf ("[Press enter to start active timer...]\n\n");
+      getc (stdin);
+      CLEARSCREEN;
+      start_timer (atoi (active_time));
+      printf ("Finished active timer.\n");
+
+      // Break timer
+      printf ("[Press enter to start break timer...]\n\n");
+      getc (stdin);
+      CLEARSCREEN;
+      start_timer (atoi (break_time));
+      printf ("Finished break timer.\n");
+    }
+
+  return 0;
 }
